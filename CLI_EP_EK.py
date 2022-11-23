72b65602309f79a2cfcd503037b7f4fbeccac9f0
rdf154 <rizka.dewi.fatimah@mail.ugm.ac.id>
2022-11-23 13:42:19 +0700

    CLI_EP_EK.py

--- CLI_EP_EK.py
+++ CLI_EP_EK.py
@@ -4,13 +4,13 @@ import sys
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--rho', type=float, default=1.0,
-                        help='What is the density number?')
+                        help='What is the density number in kg/m^3?')
     parser.add_argument('--r', type=float, default=1.0,
-                        help='What is the radius number?')
+                        help='What is the radius number in cm?')
     parser.add_argument('--h1', type=float, default=1.0,
-                        help='What is the height before falling freely number?')
+                        help='What is the height before falling freely number in m?')
     parser.add_argument('--h2', type=float, default=1.0,
-                        help='What is the height after falling in random height number?')
+                        help='What is the height after falling in random height number in m?')
     parser.add_argument('--operation', type=str, default='Ep1', 
                         help='What operation will you gonna use?')
 
@@ -19,13 +19,13 @@ def main():
 
 def operation(args):
     if args.operation == 'Ep1':
-        Ep1 = (((args.rho*(((args.r**3)*4/3)*22/7))*9.806)*args.h1)
+        Ep1 = (((args.rho*((((args.r*10**-2)**3)*4/3)*22/7))*9.806)*args.h1)
         return Ep1
     elif args.operation == 'Ep2':
-        Ep2 = (((args.rho*(((args.r**3)*4/3)*22/7))*9.806)*args.h2)
+        Ep2 = (((args.rho*((((args.r*10**-2)**3)*4/3)*22/7))*9.806)*args.h2)
         return Ep2
     elif args.operation == 'Ek2':
-        Ek2 = (((args.rho*(((args.r**3)*4/3)*22/7))*9.806)*args.h1)-(((args.rho*(((args.r**3)*4/3)*22/7))*9.806)*args.h2)
+        Ek2 = (((args.rho*((((args.r*10**-2)**3)*4/3)*22/7))*9.806)*args.h1)-(((args.rho*(((args.r**3)*4/3)*22/7))*9.806)*args.h2)
         return Ek2
 
 if __name__ == '__main__':
